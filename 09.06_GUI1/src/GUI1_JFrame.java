0ab562a940754c5aef2848d4385cedd686a812f2
OSY <dhtmddus55@naver.com>
2019-09-06 09:07:29 +0900

    어쩌다 윈도우 + label

--- 09.06_GUI1/src/GUI1_JFrame.java
+++ 09.06_GUI1/src/GUI1_JFrame.java
@@ -4,6 +4,9 @@ public class GUI1_JFrame {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+        JLabel label = new JLabel("아싸 어쩌다 금요일");
+
+        frame.add(label); //frame에다가 label text를 갖다가 붙임,   이 글씨는 왼쪽 가운데에 표시가 되어있음
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //X버튼 -> 끝내기
